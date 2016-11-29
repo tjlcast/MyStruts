104370f23605828516593e66d9cc65b41c855ca9
tangjialiang <phx108@sina.com>
2016-11-29 12:34:30 +0800

    continue

---
+++ src/bupt/tjlcast/study/day30/a_interceptor/HelloInterceptor.java
@@ -0,0 +1,45 @@
+package bupt.tjlcast.study.day30.a_interceptor;
+
+import com.opensymphony.xwork2.ActionInvocation;
+import com.opensymphony.xwork2.interceptor.Interceptor;
+
+public class HelloInterceptor implements Interceptor {
+
+	private static final long serialVersionUID = 1L;
+
+	public HelloInterceptor() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	/* 
+	 * 启动服务器时执行
+	 */
+	@Override
+	public void init() {
+		System.out.println("启动拦截器");
+	}
+
+	/* 
+	 * 拦截器业务处理： 在执行Action之前
+	 */
+	@Override
+	public String intercept(ActionInvocation invocation) throws Exception {
+		// 调用下一个拦截器或执行Action （相当与chain.doFilter）
+		// 读取的是：execute 方法的返回值
+		System.out.println("执行Action之前");
+		
+		String resultFlag = invocation.invoke() ;
+		
+		System.out.println("拦截器，业务处理结束, resultFlag:" + resultFlag);
+		
+		return resultFlag;
+	}
+
+}
---
+++ src/bupt/tjlcast/study/day30/utils/JdbcUtils.java
@@ -0,0 +1,10 @@
+package bupt.tjlcast.study.day30.utils;
+
+public class JdbcUtils {
+
+	// 初始化连接池
+	private static DataSource = 
+	
+	public 
+
+}
